7aa945c17b2a3473eb1627a27c81ed6fcd76e8c4
Frooge <jaantaros@gmail.com>
2022-04-05 13:13:26 +0800

    Added directories + core functions

---
+++ budiga_app/Core/ObeservableObject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace budiga_app.Core
+{
+    class ObeservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
