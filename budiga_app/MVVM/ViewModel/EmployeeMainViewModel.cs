170af2bcc2af4ac1aefec1027192840568e12efa
Frooge <jaantaros@gmail.com>
2022-04-07 20:33:22 +0800

    added employee navbar

--- budiga_app/MVVM/ViewModel/EmployeeMainViewModel.cs
+++ budiga_app/MVVM/ViewModel/EmployeeMainViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using budiga_app.Core;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -25,8 +26,15 @@ namespace budiga_app.MVVM.ViewModel
 
         public EmployeeMainViewModel()
         {
-            ScanVM = new ScannerViewModel();
-            CurrentView = ScanVM;
+            try
+            {
+                ScanVM = new ScannerViewModel();
+                CurrentView = ScanVM;
+            } catch (Exception ex)
+            {
+                Debug.WriteLine(ex.StackTrace);
+            }
+            
         }
     }
 }
