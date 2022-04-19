880295037d22bc9ecc4efc2e7b711b911ed92856
Jomar Leaño <69794504+therealmai@users.noreply.github.com>
2022-04-19 09:30:11 +0800

    db connection new class (#2)

---
+++ budiga_app/dbConn.cs
@@ -0,0 +1,33 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace budiga_app
+{
+    class dbConn
+    {
+        public static MySqlConnection conn;
+        public void connection()
+        {
+            string connectionString = "datasource=127.0.0.1;port=3306;username=root;password=;database=budiga_app;";
+            conn = new MySqlConnection(connectionString);
+            
+            try
+            {
+                conn.Open(); 
+            }catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+        
+        public void dispose()
+        {
+            conn.Close();
+        }
+    }
+}
