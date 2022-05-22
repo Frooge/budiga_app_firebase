a76ed9c7a4d0e944a7771131a89ec827fa45b38f
yd_blank <email@example.com>
2022-05-22 23:53:41 +0800

    search item for invoice history

--- budiga_app/MVVM/ViewModel/InvoiceHistoryViewModel.cs
+++ budiga_app/MVVM/ViewModel/InvoiceHistoryViewModel.cs
@@ -1,7 +1,9 @@
-﻿using budiga_app.DataAccess;
+﻿using budiga_app.Core;
+using budiga_app.DataAccess;
 using budiga_app.MVVM.Model;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,17 +14,31 @@ namespace budiga_app.MVVM.ViewModel
     {
         private InvoiceRepository invoiceRepository;
         public InvoiceModel Invoice { get; set; }
-        
+
+        private InvoiceModel _invoice;
+
+        public RelayCommand SearchItemCommand { get; set; }
         public InvoiceHistoryViewModel()
         {
-            Invoice = new InvoiceModel();
-            invoiceRepository = new InvoiceRepository();            
+            _invoice = new InvoiceModel();
+            Invoice = _invoice;
+            invoiceRepository = new InvoiceRepository();
+            SearchItemCommand = new RelayCommand(param => SearchItem((string)param));
             GetAll();
         }
 
         private void GetAll()
         {
-            Invoice.InvoiceRecords = invoiceRepository.GetAllInvoice();
+            _invoice.InvoiceRecords = invoiceRepository.GetAllInvoice();
+            Invoice.InvoiceRecords = _invoice.InvoiceRecords;
+        }
+
+        private void SearchItem(string searchTxt = "")
+        {
+            Invoice.InvoiceRecords = new ObservableCollection<InvoiceModel>(
+                _invoice.InvoiceRecords.Where(i => i.InvoiceOrderRecords.Where(o => o.Item.Barcode.ToLower().Contains(searchTxt.ToLower()))
+                || i.InvoiceOrderRecords.Where(o => o.Item.Name.ToLower().Contains(searchTxt.ToLower()))
+                || i.InvoiceOrderRecords.Where(o => o.Item.Brand.ToLower().Contains(searchTxt.ToLower())));
         }
     }
 }
