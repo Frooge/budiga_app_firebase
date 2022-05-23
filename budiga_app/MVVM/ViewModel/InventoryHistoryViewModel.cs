89debd99d99817fe4150ef49d6f6b58b3342849c
Frooge <jaantaros@gmail.com>
2022-05-23 15:39:30 +0800

    Added item history view and viewmodel

---
+++ budiga_app/MVVM/ViewModel/InventoryHistoryViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace budiga_app.MVVM.ViewModel
+{
+    public class InventoryHistoryViewModel
+    {
+        public InventoryHistoryViewModel()
+        {
+
+        }
+    }
+}
