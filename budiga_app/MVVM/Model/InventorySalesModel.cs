29b1934eb6e524a4304cc82c21bb442455d07d5a
therealmai <jomar35leano@gmail.com>
2022-05-30 13:34:21 +0800

    Sales Model changed

--- budiga_app/MVVM/Model/InventorySalesModel.cs
+++ budiga_app/MVVM/Model/InventorySalesModel.cs
@@ -21,7 +21,7 @@ namespace budiga_app.MVVM.Model
         public int Id { get { return _id; } set { _id = value; OnPropertyChanged("Id"); } }
         public int UnitsSold { get { return _unitsSold; } set { _unitsSold = value; OnPropertyChanged("UnitsSold"); } }
         public float TotalSales { get { return _totalSales; } set { _totalSales = value; OnPropertyChanged("TotalSales"); } }
-        public int TotalTransaction { get { return _totalTransactions; } set { _totalTransactions = value; OnPropertyChanged("TotalTransactions"); } }
+        public int TotalTransaction { get { return _totalTransactions; } set { _totalTransactions = value; OnPropertyChanged("TotalTransaction"); } }
         public string Date { get { return _date; } set { _date = value; OnPropertyChanged("Date"); } }
         public ItemModel Item { get { return _item; } set { _item = value; OnPropertyChanged("Item"); } }
         public ObservableCollection<InventorySalesModel> InventorySales { get { return _inventorySales; } set { _inventorySales = value; OnPropertyChanged("InventorySales"); } }
